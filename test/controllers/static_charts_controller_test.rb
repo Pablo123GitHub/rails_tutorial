624fab02f85d40d9d22de5986a86ffba949a33c1
Pablo <paul.ladiv123@gmail.com>
2018-12-30 19:27:23 +0100

    refactor test with string interpolation

--- test/controllers/static_charts_controller_test.rb
+++ test/controllers/static_charts_controller_test.rb
@@ -1,21 +1,25 @@
 require 'test_helper'
 
 class StaticChartsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Charts Heaven"
+  end 
+
   test "should get home" do
     get static_charts_home_url
     assert_response :success
-    assert_select "title", "Home | Charts Heaven"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_charts_help_url
     assert_response :success
-    assert_select "title", "Help | Charts Heaven"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_charts_about_url
     assert_response :success 
-    assert_select "title", "About | Charts Heaven"
+    assert_select "title", "About | #{@base_title}"
   end 
 end

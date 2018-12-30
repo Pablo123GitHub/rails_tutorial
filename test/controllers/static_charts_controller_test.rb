921f4e21f66b9813e43fae41bbb9b299973d4c96
Pablo <paul.ladiv123@gmail.com>
2018-12-30 19:16:24 +0100

    run experience by removing application layout partial to use own HTML in help/about/home views

--- test/controllers/static_charts_controller_test.rb
+++ test/controllers/static_charts_controller_test.rb
@@ -4,16 +4,18 @@ class StaticChartsControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_charts_home_url
     assert_response :success
+    assert_select "title", "Home | Charts Heaven"
   end
 
   test "should get help" do
     get static_charts_help_url
     assert_response :success
+    assert_select "title", "Help | Charts Heaven"
   end
 
   test "should get about" do
     get static_charts_about_url
     assert_response :success 
+    assert_select "title", "About | Charts Heaven"
   end 
-
 end
